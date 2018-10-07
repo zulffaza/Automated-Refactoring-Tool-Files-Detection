7c2b8976bedd92878a3e23c66e8e9139056000a5
Faza Zulfika P P <zulffaza@gmail.com>
2018-10-07 07:35:06 +0700

    Add feature files detection (single path and multi path support)

---
+++ src/main/java/com/finalproject/automated/refactoring/tool/files/detection/configuration/AsyncConfig.java
@@ -0,0 +1,15 @@
+package com.finalproject.automated.refactoring.tool.files.detection.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author Faza Zulfika P P
+ * @version 1.0.0
+ * @since 7 October 2018
+ */
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
---
+++ src/main/java/com/finalproject/automated/refactoring/tool/files/detection/service/FilesDetection.java
@@ -0,0 +1,20 @@
+package com.finalproject.automated.refactoring.tool.files.detection.service;
+
+import com.finalproject.automated.refactoring.tool.files.detection.model.FileModel;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Faza Zulfika P P
+ * @version 1.0.0
+ * @since 6 October 2018
+ */
+
+public interface FilesDetection {
+
+    List<FileModel> detect(@NonNull String path);
+
+    Map<String, List<FileModel>> detect(@NonNull List<String> paths);
+}
